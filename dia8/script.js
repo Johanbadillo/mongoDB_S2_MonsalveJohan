c3967c9e050d86a5e71f51c7cadc890e6a11b30f
johan <johanbadillomj@gmail.com>
2025-10-16 20:24:53 -0500

    feat: :sparkles: UDPATE en dia 8 and format en resources

--- dia8/script.js
+++ dia8/script.js
@@ -4,12 +4,17 @@ db.createCollection("ubicacion");
 db.createCollection("tipoEstable");
 db.createCollection("estable");
 db.createCollection("correo");
+db.createCollection("telefono");
+db.createCollection("niveles");
+db.createCollection("grados");
 
 db.ubicacion.createIndex({ idUbica: 1 }, { unique: true });
 db.tipoEstable.createIndex({ idTipoEstable: 1 }, { unique: true });
 db.estable.createIndex({ idEstable: 1 }, { unique: true });
-db.correo.createIndex({ idEstable: 1 }, { unique: true });
-
+db.correo.createIndex({idEstable: 1 }, { unique: true });
+db.telefono.createIndex({idEstable: 1 }, { unique: true });
+db.niveles.createIndex({idEstable: 1 }, { unique: true });
+db.grados.createIndex({idEstable: 1 }, { unique: true });
 
 db.dataBruto.aggregate([
     {
@@ -70,27 +75,31 @@ db.dataBruto.aggregate([
     }
 ]);
 
-db.dataBruto.aggregate([
+
+
+// si se intenta hacer de manera contraria genera muchos errores(no se porque es) en los cuales que salen que las llaves son duplicadas o no deja hacer cambios
+db.estable.aggregate([
     {
         $lookup: {
-            from: "estable",
-            localField: "idEstable",
-            foreignField: "codMuni",
-            as: "municipioInfo"
+            from: "dataBruto",
+            localField: "nomEstable",
+            foreignField: "nombreestablecimiento",
+            as: "infoCorreo"
         }
     },
     {
+        $unwind: "$infoCorreo"
+    },
+    {
         $project: {
-            _id: 0,
-            nomEstable: "$nombreestablecimiento",
-            numSedes: "$numero_de_Sedes",
-            nomRector: "$nombre_Rector",
+            _id: "$idEstable",
+            correo: "$infoCorreo.correo_Electronico",
             idEstable: "$_id"
         }
     },
     {
         $merge: {
-            into: "estable",
+            into: "correo",
             on: "idEstable",
             whenMatched: "merge",
             whenNotMatched: "insert"
@@ -98,3 +107,89 @@ db.dataBruto.aggregate([
     }
 ]);
 
+db.estable.aggregate([
+    {
+        $lookup: {
+            from: "dataBruto",
+            localField: "nomEstable",
+            foreignField: "nombreestablecimiento",
+            as: "infoTelefono"
+        }
+    },
+    {
+        $unwind: "$infoTelefono"
+    },
+    {
+        $project: {
+            _id: "$idEstable",
+            correo: "$infoTelefono.telefono",
+            idEstable: "$_id"
+        }
+    },
+    {
+        $merge: {
+            into: "telefono",
+            on: "idEstable",
+            whenMatched: "merge",
+            whenNotMatched: "insert"
+        }
+    }
+]);
+
+db.estable.aggregate([
+    {
+        $lookup: {
+            from: "dataBruto",
+            localField: "nomEstable",
+            foreignField: "nombreestablecimiento",
+            as: "infoNiveles"
+        }
+    },
+    {
+        $unwind: "$infoNiveles"
+    },
+    {
+        $project: {
+            _id: "$idEstable",
+            correo: "$infoNiveles.niveles",
+            idEstable: "$_id"
+        }
+    },
+    {
+        $merge: {
+            into: "niveles",
+            on: "idEstable",
+            whenMatched: "merge",
+            whenNotMatched: "insert"
+        }
+    }
+]);
+
+db.estable.aggregate([
+    {
+        $lookup: {
+            from: "dataBruto",
+            localField: "nomEstable",
+            foreignField: "nombreestablecimiento",
+            as: "infogrados"
+        }
+    },
+    {
+        $unwind: "$infogrados"
+    },
+    {
+        $project: {
+            _id: "$idEstable",
+            correo: "$infogrados.niveles",
+            idEstable: "$_id"
+        }
+    },
+    {
+        $merge: {
+            into: "niveles",
+            on: "idEstable",
+            whenMatched: "merge",
+            whenNotMatched: "insert"
+        }
+    }
+]);
--- resources/Pedro.js
+++ resources/Pedro.js
@@ -1,182 +1,185 @@
 use incautaciones_db2;
 
-    /*
-    - `incautaciones(fecha_hecho, cod_muni, cantidad, id_unidad)`
-    - `municipios(cod_muni, nombre_muni, cod_depto)`
-    - `departamentos(cod_depto, nombre_depto)`
-    - `unidades(_id, nombre_unidad)`
-    */
-    //CUIDADO!!!! Eliminación de datos "crudos" o sin procesar
-    db.incautaciones_raw.drop();
+/*
+- `incautaciones(fecha_hecho, cod_muni, cantidad, id_unidad)`
+- `municipios(cod_muni, nombre_muni, cod_depto)`
+- `departamentos(cod_depto, nombre_depto)`
+- `unidades(_id, nombre_unidad)`
+*/
+//CUIDADO!!!! Eliminación de datos "crudos" o sin procesar
+db.incautaciones_raw.drop();
     show collections;
-    //Contar los documentos que están almacenados
-    db.incautaciones.aggregate([
-        {
-            $count:"total_registros"//Crea una clave "total_registros", la cual muestra el total de los documentos registrados en la colección
+//Contar los documentos que están almacenados
+db.incautaciones.aggregate([
+    {
+        $count: "total_registros"//Crea una clave "total_registros", la cual muestra el total de los documentos registrados en la colección
+    }
+]);
+//Cantidad total incautada
+db.incautaciones.find();
+db.incautaciones.findOne();
+db.incautaciones.aggregate([
+    {
+        $group: {
+            _id: null,
+            total_cantidad: {
+                $sum: "$cantidad"
             }
-        ]);
-    //Cantidad total incautada
-    db.incautaciones.find();
-    db.incautaciones.findOne();
-    db.incautaciones.aggregate([
-        {
-            $group:{
-                _id:null,
-                total_cantidad:{
-                    $sum:"$cantidad"
-                    }
-                }
-            }
-        ]);
+        }
+    }
+]);
 
-    //Agrupar por año
-    db.incautaciones.aggregate([
-        {
-            $addFields:{
-                anio:{$year:'$fecha_hecho'}
-                }
-            },
-        {
-            $group:{
-                _id:'$anio',
-                total:{$sum:'$cantidad'}
-                }
-            },
-        {
-            $sort:{
-                _id:1
-                }
-            }
-        ]);
+//Agrupar por año
+db.incautaciones.aggregate([
+    {
+        $addFields: {
+            anio: { $year: '$fecha_hecho' }
+        }
+    },
+    {
+        $group: {
+            _id: '$anio',
+            total: { $sum: '$cantidad' }
+        }
+    },
+    {
+        $sort: {
+            _id: 1
+        }
+    }
+]);
 
-    //Agrupación por año y mes
-    db.incautaciones.aggregate([
-        {
-            $group:{
-                _id:{
-                    anio:{$year:'$fecha_hecho'},
-                    mes:{$month:"$fecha_hecho"}
-                    },
-                total:{
-                    $sum:'$cantidad'
-                    }
-                }
+//Agrupación por año y mes
+db.incautaciones.aggregate([
+    {
+        $group: {
+            _id: {
+                anio: { $year: '$fecha_hecho' },
+                mes: { $month: "$fecha_hecho" }
             },
-        {
-            $sort:{
-                "_id.anio":1,"_id.mes":1
-                }
+            total: {
+                $sum: '$cantidad'
             }
-        ]);
+        }
+    },
+    {
+        $sort: {
+            "_id.anio": 1, "_id.mes": 1
+        }
+    }
+]);
 
-    //Top 5 Municipios de mayor incautación
-    db.incautaciones.aggregate([
-        {
-            $group:{
-                _id:"$cod_muni",
-                total:{$sum:'$cantidad'}
-                }
-            },{
-            $sort:{total:-1}
-            },{
-            $limit:5
-            },{
-            $lookup:{
-                from:"municipios",
-                localField:"_id",
-                foreignField:"cod_muni",
-                as:"muni"
-                }
-            },{
-            $unwind:'$muni'
-            },{
-            $project:{
-                municipio:"$muni.nombre_muni",
-                total:1,
-                _id:0
-                }
-            }
+//Top 5 Municipios de mayor incautación
+db.incautaciones.aggregate([
+    {
+        $group: {
+            _id: "$cod_muni",
+            total: { $sum: '$cantidad' }
+        }
+    }, {
+        $sort: { total: -1 }
+    }, {
+        $limit: 5
+    }, {
+        $lookup: {
+            from: "municipios",
+            localField: "_id",
+            foreignField: "cod_muni",
+            as: "muni"
+        }
+    }, {
+        $unwind: '$muni'
+    }, {
+        $project: {
+            municipio: "$muni.nombre_muni",
+            total: 1,
+            _id: 0
+        }
+    }
 
-        ]);
-    //Promedio por municipio --> Top 10 promedios de incautacion
-    //por municipio
-    db.incautaciones.aggregate(
-        [{
-            $group:{
-                _id: "$cod_muni",
-                promedio: {$avg: "$cantidad"}}
-            },
-            {$lookup:{
-                from: "municipios",
-                localField: "_id",
-                foreignField: "cod_muni",
-                as: "municipioSerio"
-                } },
-            {$unwind: "$municipioSerio"},{
-            $project:{
-                municipio:'$municipioSerio.nombre_muni',
-                total:'$promedio'
-                }
-            },{
-            $sort:{total :-1}
-            },
-            {
-                $limit:10
-                }]);
+]);
+//Promedio por municipio --> Top 10 promedios de incautacion
+//por municipio
+db.incautaciones.aggregate(
+    [{
+        $group: {
+            _id: "$cod_muni",
+            promedio: { $avg: "$cantidad" }
+        }
+    },
+    {
+        $lookup: {
+            from: "municipios",
+            localField: "_id",
+            foreignField: "cod_muni",
+            as: "municipioSerio"
+        }
+    },
+    { $unwind: "$municipioSerio" }, {
+        $project: {
+            municipio: '$municipioSerio.nombre_muni',
+            total: '$promedio'
+        }
+    }, {
+        $sort: { total: -1 }
+    },
+    {
+        $limit: 10
+    }]);
 
-    //Ranking de Departamentos por Incautación
-    db.evento.aggregate([
-        {
-            $lookup:{
-                from:'municipios',
-                localField:"cod_muni",
-                foreignField:"codMuni",
-                as:'muni'
-                }
-            },{
-            $unwind:'$muni'
-            },
-        {
-            $lookup:{
-                from:'departamentos',
-                localField:'muni.codDepto',
-                foreignField:'codDepto',
-                as:'depto'
-                }
-            },{
-            $unwind:'$depto'
-            },{
-            $group:{
-                _id:{ departamento:'$depto.nomDepto', municipio: "$muni.nomMuni"},
-                total:{$sum:'$cantidad'}
-                }
-            },{
-            $sort:{total:-1}
-            }
-        ]);
+//Ranking de Departamentos por Incautación
+db.evento.aggregate([
+    {
+        $lookup: {
+            from: 'municipios',
+            localField: "cod_muni",
+            foreignField: "codMuni",
+            as: 'muni'
+        }
+    }, {
+        $unwind: '$muni'
+    },
+    {
+        $lookup: {
+            from: 'departamentos',
+            localField: 'muni.codDepto',
+            foreignField: 'codDepto',
+            as: 'depto'
+        }
+    }, {
+        $unwind: '$depto'
+    }, {
+        $group: {
+            _id: { departamento: '$depto.nomDepto', municipio: "$muni.nomMuni" },
+            total: { $sum: '$cantidad' }
+        }
+    }, {
+        $sort: { total: -1 }
+    }
+]);
 
-    //Ranking de los municipios con
-    //más incautaciones que empiezan por "Puerto"
-    db.incautaciones.aggregate([
-        {
-            $lookup:{
-                from:'municipios',
-                localField:"cod_muni",
-                foreignField:"cod_muni",
-                as:'muni'
-                }
-            },
-        {$unwind:'$muni'}
-        ,{
-            $match:{
-                'muni.nombre_muni':{$regex:/^Puerto/i}
-            }
-            },{
-            $sort:{total:-1}
-            },{
-            $group:{
-                _id:'$muni.nombre_muni',
-                total:{$sum:'$cantidad'}
-            }
-            }]);
+//Ranking de los municipios con
+//más incautaciones que empiezan por "Puerto"
+db.incautaciones.aggregate([
+    {
+        $lookup: {
+            from: 'municipios',
+            localField: "cod_muni",
+            foreignField: "cod_muni",
+            as: 'muni'
+        }
+    },
+    { $unwind: '$muni' }
+    , {
+        $match: {
+            'muni.nombre_muni': { $regex: /^Puerto/i }
+        }
+    }, {
+        $sort: { total: -1 }
+    }, {
+        $group: {
+            _id: '$muni.nombre_muni',
+            total: { $sum: '$cantidad' }
+        }
+    }]);
 
